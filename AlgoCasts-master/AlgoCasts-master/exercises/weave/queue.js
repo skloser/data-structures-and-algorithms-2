cec394f19571f2630d857459119516f175b5d71e
Krum Bagashev <krum.bagashev@pharmalex.com>
2023-01-30 16:21:24 +0200

    Added queue weaving

--- AlgoCasts-master/AlgoCasts-master/exercises/weave/queue.js
+++ AlgoCasts-master/AlgoCasts-master/exercises/weave/queue.js
@@ -5,17 +5,21 @@
 // removing it.
 
 class Queue {
-  constructor() {
-    this.data = [];
-  }
+	constructor() {
+		this.data = [];
+	}
 
-  add(record) {
-    this.data.unshift(record);
-  }
+	peek() {
+		return this.data[this.data.length - 1];
+	}
 
-  remove() {
-    return this.data.pop();
-  }
+	add(record) {
+		this.data.unshift(record);
+	}
+
+	remove() {
+		return this.data.pop();
+	}
 }
 
 module.exports = Queue;
